9ac1d6b73dcdee959fb862cbc9bb47330c68f728
Mogakamo <mogaka.amo254@gmail.com>
2021-10-15 11:58:32 +0000

    added axios and used it

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,32 @@
   </div>
 </template>
 
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      ApiKey: "9595f7953f14d0ebc0d94574fad520c4",
+      city: "Detroit",
+    };
+  },
+  methods: {
+    getCurrentWeather() {
+      axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.ApiKey}`
+      );
+    },
+  },
+};
+</script>
+
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family:'JetBrains Mono', monospace;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "JetBrains Mono", monospace;
+}
 </style>
